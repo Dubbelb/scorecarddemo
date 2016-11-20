564cb025ed21e274741e49a6c507d98c9e4b6d27
Alexander Croner <alexander@croner.nu>
2016-11-20 22:00:25 +0100

    First commit

---
+++ src/ScoreCard/Controllers/ScoreCard/ScoreCardController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ScoreCard.Controllers
+{
+    public class ScoreCardController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Route("data")]
+        [ResponseCache(Location = ResponseCacheLocation.None, NoStore = true)]
+        public ActionResult ScoreCardData()
+        {
+            string allText = System.IO.File.ReadAllText(@"C:\Users\Alex\documents\visual studio 2015\Projects\ScoreCard\src\ScoreCard\wwwroot\images\data.json");
+
+            object jsonObject = JsonConvert.DeserializeObject(allText);
+            return Json(jsonObject);
+        }
+    }
+}
---
+++ src/ScoreCard/wwwroot/js/ScoreCard.jsx
@@ -0,0 +1,179 @@
+﻿var ScoreCardBox = React.createClass({
+    loadDataFromServer: function () {
+        var xhr = new XMLHttpRequest();
+        xhr.open('get', this.props.url, true);
+        xhr.onload = function () {
+            var data = JSON.parse(xhr.responseText);
+            this.setState({ data: data });
+        }.bind(this);
+        xhr.send();
+    },
+    componentDidMount: function () {
+        this.loadDataFromServer();
+        window.setInterval(this.loadDataFromServer, this.props.pollInterval);
+    },
+    getInitialState: function () {
+        return { data: [] };
+    },
+    render: function () {
+        return (
+          <div className="scoreCardBox">
+            <RoundBox rounds={this.state.data.Rounds} />
+            <PlayerList data={this.state.data.Rounds} />
+          </div>
+      );
+    }
+});
+var RoundBox = React.createClass({
+    render: function () {
+        debugger
+        if (this.props.rounds) {
+            var courseData = this.props.rounds[0].Course;
+
+            var holeTdNodes = courseData.Holes.map(function (hole) {
+                return (
+                    <TdNode value={hole.HoleNumber} key={hole.HoleNumber} />
+                    )
+            });
+            var parTdNodes = courseData.Holes.map(function (hole) {
+                return (
+                    <TdNode value={hole.Par} key={hole.HoleNumber} />
+                    )
+            });
+            return (
+                <div className="courseInfo-container">
+                    <div className="courseInfo-name"><strong>{courseData.ClubName} - {courseData.CourseName}</strong></div>
+                    <div className="courseInfoRows-container">
+                        <table className="courseInfo-table table-responsive table-striped">
+                            <tbody>
+                                <tr>
+                                    <td>Hål</td>{holeTdNodes}
+                                </tr>
+                                <tr>
+                                    <td>Par</td>{parTdNodes}
+                                </tr>
+                                <tr>
+                                    <td>Index</td>
+                                </tr>
+                                <tr>
+                                    <td>Tee</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            );
+        }
+        else {
+            return null;
+        }
+    }
+});
+var TdNode = React.createClass({
+    render: function () {
+        return (
+            <td>{this.props.value}</td>
+            );
+    }
+});
+var PlayerList = React.createClass({
+    _renderPlayers: function () {
+        var playerNodes = [];
+        if (this.props.data) {
+            playerNodes = this.props.data.map(function (playerData) {
+                return playerData.TeeTimes.map(function (data) {
+                    return data.Teams.map(function (data2) {
+                        return (
+                            <PlayerBox data={data2} key={data2.TeamExternalId} />
+                        );
+                    })
+                });
+            });
+        }
+        return playerNodes;
+    },
+    _renderHoleHeaders: function () {
+        var holeHeaders = [];
+        if (this.props.data) {
+            holeHeaders = this.props.data[0].Course.Holes.map(function (hole) {
+                return (
+                    <HoleResultHeader hole={hole.HoleNumber} key={hole.HoleNumber} />
+                    );
+            });
+        }
+        return holeHeaders;
+    },
+    render: function () {
+        return (
+          <div className="playerList">
+              <div className="playerListHeader">
+                  <div className="playerBox-playerInfo header"><strong>Players</strong></div>
+                <div className="playData-header">{this._renderHoleHeaders()}</div>
+              </div>
+              
+              {this._renderPlayers()}
+          </div>
+      );
+    }
+});
+var HoleResultHeader = React.createClass({
+    render: function () {
+        return (
+            <div className="hole-name"><strong>{this.props.hole}</strong></div>
+            );
+    }
+});
+var PlayerBox = React.createClass({
+    _renderResultNodes: function () {
+        var data = this.props.data;
+        var resultNodes = data.TeamHoleResults.map(function (result) {
+            return (
+                <TeamHoleResultBox data={result} key={result.CourseHoleExternalId} />
+                );
+        });
+        return resultNodes;
+    },
+    render: function () {
+        var data = this.props.data;
+        
+        return (
+            <div className="playerBox">
+                <div className="playerBox-playerInfo">
+                    <div className="playerBox-playerInfo-upperRow">
+                        <div className="playerBox-name">
+                            <strong>{data.TeamName}</strong>
+                        </div>
+                    </div>
+                    <div className="playerBox-playerInfo-lowerRow">
+                        <span className="playerBox-playerInfo-homeClub">
+                            {data.TeamExternalId}
+                        </span>
+                        &nbsp;
+                        <strong>
+                            HCP: {data.PlayingHCP}
+                        </strong>
+                    </div>
+                </div>
+
+                <div className="playerBox-playData">
+                    {this._renderResultNodes()}
+                </div>
+            </div>
+        );
+    }
+});
+var TeamHoleResultBox = React.createClass({
+    render: function () {
+        return (
+            <div className="teamHoleResultContainer">
+                <div className="teamHoleNumber">{this.props.data.Strokes}</div>
+                <div>{this.props.data.Score}</div>
+            </div>
+            );
+    }
+});
+
+ReactDOM.render(
+  <ScoreCardBox url="/data" pollInterval={20000} />,
+  document.getElementById('content')
+);
